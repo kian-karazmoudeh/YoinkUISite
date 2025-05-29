f7f6d2cc5421c97d43767e2931fe1f628364d01a
Kian Karazmoudeh <kian.karazmoudeh@gmail.com>
2025-05-29 10:27:58 +0800

    prelaunch sign up form

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,62 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+  const supabase = createClient()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function isEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  const handleSignUp = async () => {
+    if (!email) {
+      alert("Please enter your email address.");
+      return;
+    } else if (!isEmail(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    } else {
+      const { data, error } = await supabase.from("prelaunch").insert([{ email }]);
+      if (error) {
+        console.error("Error inserting email:", error); 
+        alert("There was an error signing up. Please try again later.");
+        return;
+      }
+      console.log("Email inserted successfully:", data);
+      router.push("/thanks");
+    }
+  }
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex space-x-5 items-center mx-auto">
+          <Image src={"/yoinkUI.svg"} alt="YoinkUI Logo" width={60} height={60} className="md:w-[60px] md:h-[60px] w-10 h-10" />
+          <h1 className="md:text-6xl text-4xl font-bold text-center w-full">YoinkUI</h1>
         </div>
+        <p className="text-zinc-700 text-center w-full">Copy any landing page with just one click. <br />
+          Sign up to get notified when we launch!
+        </p>
+        <form className="space-x-3" onSubmit={(e) => {e.preventDefault()}}>
+          <input className="px-4 py-2 rounded-md border-1 border-zinc-500" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button
+            className="px-4 py-2 bg-blue-500 rounded-md text-white font-semibold"
+            onClick={handleSignUp}>Sign Up</button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="flex flex-col items-center justify-center row-start-3">
+        <p className="text-zinc-500 text-sm">Copyright © 2025 YoinkUI®. All rights reserved.</p>
       </footer>
+      <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
+        <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+      </div>
     </div>
   );
 }
---
+++ app/thanks/page.tsx
@@ -0,0 +1,21 @@
+const ThanksPage = () => {
+    return ( 
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+              <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <div className="flex space-x-5 items-center mx-auto">
+                  <h1 className="md:text-6xl text-4xl font-bold text-center w-full">Thank you!</h1>
+                </div>
+                <p className="text-zinc-700 text-center w-full">You will be the first to hear when we launch!</p>
+                
+              </main>
+              <footer className="flex flex-col items-center justify-center row-start-3">
+                <p className="text-zinc-500 text-sm">Copyright © 2025 YoinkUI®. All rights reserved.</p>
+              </footer>
+              <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
+                <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+              </div>
+            </div>
+     );
+}
+ 
+export default ThanksPage;

6f6946efbb053cf75581288bc96985cf743033b8
Kian Karazmoudeh <kian.karazmoudeh@gmail.com>
2025-05-29 10:42:32 +0800

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <title>YoinkUI</title>
+        <meta name="description" content="Copy any UI in just one click" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>

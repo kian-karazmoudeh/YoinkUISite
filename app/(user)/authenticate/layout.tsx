c06ffc61f755b216e45333f4a67e0c1fdb36cda4
Kian Karazmoudeh <kian.karazmoudeh@gmail.com>
2025-07-02 15:32:03 +0800

    prelaunch fixes 6

--- app/(user)/authenticate/layout.tsx
+++ app/(user)/authenticate/layout.tsx
@@ -1,12 +1,17 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Authenticate | YoinkUI",
   description: "Log in or Sign up to continue to YoinkUI",
 };
 
-const UserLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full !h-[100vh]">
+      <Suspense>{children}</Suspense>
+    </div>
+  );
 };
 
-export default UserLayout;
+export default AuthLayout;
